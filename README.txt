9f833064d74abca491734cb0557c0fb0d2db2f64
zzsedward <zzhe.wen@gmail.com>
2016-11-19 21:09:58 +0000

    utlm scatter, connect functions

---
+++ README.txt
@@ -0,0 +1,81 @@
+vector<mesh> mesh_obj;
+
+
+
+
+
+int main()
+{
+	vector<mesh> mesh1;
+	mesh1.reserve(number_of_cells);
+
+	for (int i_ts=0;i_ts<timesteps;++i_ts){
+		
+		scatter;
+		
+		connect;
+	
+	}
+}
+
+class mesh{
+
+	double node_centre[2];
+	double edge[3];
+	double neig[3];
+	double edge_length[3];
+	double link_length[3];
+	double Vlinki[3];
+	double Vstubr[3];
+	double Impedance;
+	double epsilonr;
+	double mur;
+	double area;
+	
+	mesh(int &_numbe_edge, vector<int> _vertice_index,)
+	int number_edges;
+	vector<int> vertice_index; 
+
+	
+}
+
+ 
+	
+void scatter()
+{
+	int no_edge(sizeof(vector(mesh_edge));
+	int no_edge_per_face(3);
+	
+	for(int i_edge=0;i_edge<no_edge_per_face;++i_edge)
+	{
+		Io=2*Vlinki[n]*Ylink[n];
+	}
+	
+	Vo=Io*Z;
+	 
+	
+}
+
+void connect()
+{
+	
+	int index_edge, index_neigh;
+	
+	if(Y_stub>1e10)
+	{	
+		E_i[index_edge].Vlinki=0;
+
+		H_i[index_edge].Vlinki=0;
+
+		H_x[index_edge].Vlinki=0;
+
+		H_y[index_edge].Vlinki=0;
+
+		Vlinki[index_edge]=0;
+
+		Vstub[index_edge]=0;
+
+		Vlinki[index_flip]=0;
+
+		Vstub[index_flip]=0;
+}
---
+++ utlm.cpp
@@ -0,0 +1,139 @@
+#include <string.h>
+#include <complex>
+#include <math.h>
+#include <iterator>
+#include <algorithm>
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <limits>
+#include <time.h>
+#include <functional>
+#include <vector>
+
+using namespace std;
+class mesh_face{
+
+    double node_centre[2];
+    double vertice[3];
+    double area;
+    double epsilon_relative;
+    double mu_relative;
+    double Z0;
+};
+
+class mesh_edge{
+
+    double edge_vertice[2];
+    int flip_index;
+    double midpoint[2];
+    double edgeLength;
+    double linkLength;
+    double Vlinki;
+    double Vlinkr;
+    double Vstub;
+    double Ylink;
+    double Ystub;
+
+
+};
+
+class
+int main(int argc, char* argv[])
+{
+    cout<<"Hello World!"<<endl;
+    int timestep(1e4);
+    double del_t(1e-10);
+
+    for(int it=0;it<timestep;++it){
+
+        scatter(it,&);
+
+        connect(it);
+    }
+
+
+}
+
+void scatter(const int &time_step, vector<mesh_edge> &mesh_edges, vector<mesh_face> &mesh_body)
+{
+    int no_edge=mesh_edges.size();
+    int no_face=mesh_body.size();
+    int no_edge_per_face(3);
+
+    vector<double> nodeCurrent;
+    nodeCurrent.reserve(no_face);
+	memset(&nodeCurrent[0],0,sizeof(double)*no_face);
+
+    vector<double> nodeVoltage;
+    nodeVoltage.reserve(no_face);
+	memset(&nodeVoltage[0],0,sizeof(double)*no_face);
+
+    for(int kface=0;kface<no_face;++kface){
+
+        nodeCurrent[kface]=2*(mesh_edges[kface*3+0].Vlinki*mesh_edges[kface*3+0].Ylink+mesh_edges[kface*3+1].Vlinki*mesh_edges[kface*3+1].Ylink+mesh_edges[kface*3+2].Vlinki*mesh_edges[kface*3+2].Ylink);
+        nodeVoltage[kface]=nodeCurrent[kface]*mesh_body[kface].Z0;
+
+        mesh_edges[kface*3+0].Vlinkr=nodeVoltage[kface]-mesh_edges[kface*3+0].Vlinki;
+
+        mesh_edges[kface*3+1].Vlinkr=nodeVoltage[kface]-mesh_edges[kface*3+1].Vlinki;
+
+        mesh_edges[kface*3+2].Vlinkr=nodeVoltage[kface]-mesh_edges[kface*3+2].Vlinki;
+
+    }
+
+    //-----open circuit voltage and closed circuit current------------------
+
+}
+
+void connect(const int &time_step, vector<mesh_edge> &mesh_edges, vector<mesh_face> &mesh_body, const vector<int> &boundary, const vector<int> &mesh_internal)
+{
+    //how to decide an edge is on the boundary---------------------------
+    const int no_edge(mesh_edges.size());
+    const int no_face(mesh_body.size());
+    const int no_bound_face(boundary.size());
+    const int no_internal_nodes(mesh_internal.size());
+
+    vector<double> Efield;
+    Efield.reserve(no_edge);
+    vector<double> Hfield;
+    Hfield.reserve(no_edge);
+
+    for(int it_bound_face=0;it_bound_face<no_bound_face;++it_bound_face){
+
+        const int bound_edge_index(boundary[it_bound_face]);
+
+        //how to define infinity Ystub or PEC?-------------------------
+        if(mesh_edges[bound_edge_index].Ystub>1e8){
+            Hfield[bound_edge_index]=0;
+        }
+
+    }
+
+    for(int it_internal_nodes=0;it_internal_nodes<no_internal_nodes;++it_internal_nodes){
+
+        const int internal_edge_index(mesh_internal[it_internal_nodes]);
+
+        const int flip_edge_index(mesh_edges[internal_edge_index].flip_index);
+
+        if(mesh_edges[internal_edge_index].Ystub>1e8||mesh_edges[flip_edge_index].Ystub>1e8){
+
+            mesh_edges[internal_edge_index].Vlinki=0;
+            mesh_edges[internal_edge_index].Vstub=0;
+            mesh_edges[flip_edge_index].Vlinki=0;
+            mesh_edges[flip_edge_index].Vstub=0;
+
+        }
+        else{
+
+            double Iconnect(2*(mesh_edges[internal_edge_index].Vlinkr*mesh_edges[internal_edge_index].Ylink+mesh_edges[flip_edge_index].Vlinkr*mesh_edges[flip_edge_index].Ylink+mesh_edges[internal_edge_index].Vstub*mesh_edges[internal_edge_index].Ystub+mesh_edges[flip_edge_index].Vstub*mesh_edges[flip_edge_index].Ystub));
+
+            double Ytotal(mesh_edges[internal_edge_index].Ylink+mesh_edges[internal_edge_index].Ystub+mesh_edges[flip_edge_index].Ylink+mesh_edges[flip_edge_index].Ystub);
+
+            mesh_edges[internal_edge_index].Vlinki=Iconnect/Ytotal-mesh_edges[internal_edge_index].Vlinkr;
+            mesh_edges[internal_edge_index].Vstub=Iconnect/Ytotal-mesh_edges[internal_edge_index].Vstub;
+            mesh_edges[flip_edge_index].Vlinki=Iconnect/Ytotal-mesh_edges[flip_edge_index].Vlinkr;
+            mesh_edges[flip_edge_index].Vstub=Iconnect/Ytotal-mesh_edges[flip_edge_index].Vstub;
+        }
+    }
+}
